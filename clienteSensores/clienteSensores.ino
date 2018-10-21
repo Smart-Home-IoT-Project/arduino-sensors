200a1f532e66b921f47ec01a10294f48c7e57353
Joan Ciprià <joancipria@gmail.com>
2018-10-21 12:56:39 +0200

    Implementado sensor distancia (ultrasonido)

--- clienteSensores/clienteSensores.ino
+++ clienteSensores/clienteSensores.ino
@@ -3,6 +3,7 @@
 #include <TimeLib.h>
 #include <ArduinoJson.h>
 
+// Wifi info
 const char * ssid = "Equipo_4";
 const char * password = "scrumMaster1213";
 
@@ -10,10 +11,20 @@ AsyncUDP udp;
 StaticJsonBuffer<200> jsonBuffer;                 //tamaño maximo de los datos
 JsonObject& envio = jsonBuffer.createObject();    //creación del objeto "envio"
 
+// Pines sensor distancia
+const int EchoPin = 35; 
+const int TriggerPin = 23;
+
 void setup()
 {
     Serial.begin(115200);
+
+    // Establecemos hora
     setTime (9, 15, 0, 7, 10, 2018); //hora minuto segundo dia mes año
+
+    // Sensor distancia
+    pinMode(TriggerPin, OUTPUT); 
+    pinMode(EchoPin, INPUT); 
       
     WiFi.mode(WIFI_STA);
     WiFi.begin(ssid, password);
@@ -43,14 +54,31 @@ void loop()
 {
     delay(1000);
     char texto[200];
-    
-    envio["Hora"]=hour();         //Datos introducidos en el objeto "envio"
-    envio["Minuto"]=minute();     //3 campos 
-    envio["Segundo"]=second();    //
+
+    // Get full time
+    String fullTime = String(hour())+" : "+String(+minute())+" : "+String(second());
+
+    // Store data and time
+    envio["Distancia"] = distancia(TriggerPin, EchoPin);   
+    envio["Hora"]=fullTime;         
+
       
     envio.printTo(texto);         //paso del objeto "envio" a texto para transmitirlo
 
     udp.broadcastTo(texto,1234);  //se emvía por el puerto 1234 el JSON 
                                   //como texto
     
+}
+
+// Calcular distancia
+int distancia(int TriggerPin, int EchoPin) {
+  long duracion, distanciaCm;
+  digitalWrite(TriggerPin, LOW); //nos aseguramos señal baja al principio 
+  delayMicroseconds(4); 
+  digitalWrite(TriggerPin, HIGH); //generamos pulso de 10us 
+  delayMicroseconds(10); 
+  digitalWrite(TriggerPin, LOW); 
+  duracion = pulseIn(EchoPin, HIGH); //medimos el tiempo del pulso 
+  distanciaCm = duracion * 10 / 292 / 2; //convertimos a distancia 
+  return distanciaCm;
 }
